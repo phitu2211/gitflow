ee450daef318bb839ac41c30b62682f0eb1a433d
phitu2211 <phitu2211@gmail.com>
2023-10-09 13:48:09 +0700

    features: them sinh vien continue

--- src/main/java/com/example/gitflow/HelloServlet.java
+++ src/main/java/com/example/gitflow/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello Trong!";
+        message = "Hello Trong 2!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
